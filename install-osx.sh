5a6846e4c2bbca0cf7d9b30696422a7baa1c123a
istvanv <istvanv>
2008-12-03 18:11:21 +0000

    Removed ROM files from the emulator package; reverted to downloading from an external server

--- install-osx.sh
+++ install-osx.sh
@@ -3,19 +3,17 @@
 BASE_DIR="$HOME/Library/Application Support/ep128emu"
 MAKECFG="`dirname \"$0\"`/ep128emu.app/Contents/MacOS/makecfg"
 
-if ( ! [ -e "$BASE_DIR/ep128cfg.dat" ] ) ; then
-  "$MAKECFG" -f "$BASE_DIR" ;
-else
-  "$MAKECFG" "$BASE_DIR" ;
-fi
-
 if ( ! [ -e "$BASE_DIR/roms/exos231.rom" ] ) ; then
   SAVED_CWD="`pwd`"
   mkdir -p "$BASE_DIR/roms" || exit -1
   cd "$BASE_DIR/roms" || exit -1
-  curl -o ep128emu_roms.zip http://ep128emu.enterpriseforever.org/roms/ep128emu_roms.zip
-  unzip -o ep128emu_roms.zip
-  rm -f ep128emu_roms.zip
+  curl -o ep128emu_roms.bin http://ep128emu.enterpriseforever.org/roms/ep128emu_roms.bin
   cd "$SAVED_CWD" ;
+fi
+
+if ( ! [ -e "$BASE_DIR/ep128cfg.dat" ] ) ; then
+  "$MAKECFG" -f "$BASE_DIR" ;
+else
+  "$MAKECFG" "$BASE_DIR" ;
 fi
 
