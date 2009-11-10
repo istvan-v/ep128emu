be0a77e29af692d096e7f58066225cdeebd0447e
istvanv <istvanv>
2009-11-10 17:35:17 +0000

    Temporary stub for CPC emulator class

---
+++ src/cpc464vm.hpp
@@ -0,0 +1,33 @@
+
+// ep128emu -- portable Enterprise 128 emulator
+// Copyright (C) 2003-2009 Istvan Varga <istvanv@users.sourceforge.net>
+// http://sourceforge.net/projects/ep128emu/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef EP128EMU_CPC464VM_HPP
+#define EP128EMU_CPC464VM_HPP
+
+#include "ep128emu.hpp"
+#include "ep128vm.hpp"
+
+namespace CPC464 {
+
+  typedef Ep128::Ep128VM CPC464VM;
+
+}       // namespace CPC464
+
+#endif  // EP128EMU_CPC464VM_HPP
+
