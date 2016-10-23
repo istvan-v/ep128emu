95fb0cc6410a4b794993e06c563079f913650313
IstvanV <istvan-v@users.noreply.github.com>
2016-10-23 13:45:19 +0200

    Removed automatic downloading of ROM package

--- install-osx.sh
+++ install-osx.sh
@@ -3,15 +3,7 @@
 BASE_DIR="$HOME/Library/Application Support/ep128emu"
 MAKECFG="`dirname \"$0\"`/ep128emu.app/Contents/MacOS/makecfg"
 
-if ( ! [ -e "$BASE_DIR/roms/sdext05.rom" ] ) ; then
-  SAVED_CWD="`pwd`"
-  mkdir -p "$BASE_DIR/roms" || exit -1
-  cd "$BASE_DIR/roms" || exit -1
-  curl -o ep128emu_roms-2.0.10.bin 'https://enterpriseforever.com/letoltesek-downloads/egyeb-misc/?action=dlattach;attach=16433'
-  cd "$SAVED_CWD" ;
-fi
-
-if ( ! [ -e "$BASE_DIR/ep128cfg.dat" ] ) ; then
+if ( ! [ -e "$BASE_DIR/config/ep128uk/EP_640k_EXOS24_SDEXT_utils.cfg" ] ) ; then
   "$MAKECFG" -f "$BASE_DIR" ;
 else
   "$MAKECFG" "$BASE_DIR" ;
