e895f81e370c42db62bf3aacadb6b49333b0f4d9
IstvanV <istvan-v@users.noreply.github.com>
2016-11-05 21:02:42 +0100

    Minor change to Deflate optimization

--- src/pngwrite.cpp
+++ src/pngwrite.cpp
@@ -338,7 +338,7 @@ namespace Ep128Emu {
       // and literal byte:
       size_t  nBits = bitCountTable[i + 1]
                       + huffmanEncoderL.getSymbolSize(inBuf[offs + i]);
-      if (nBits <= bestSize) {
+      if (nBits < bestSize) {
         bestSize = nBits;
         bestOffs = 0;
         bestLen = 1;
