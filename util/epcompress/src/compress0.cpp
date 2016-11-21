0b0d9563425901935529e1515bc173a006bacde3
IstvanV <istvan-v@users.noreply.github.com>
2016-11-21 13:46:55 +0100

    Use new Huffman code from comprlib.cpp, since it optimizes correctly now

--- util/epcompress/src/compress0.cpp
+++ util/epcompress/src/compress0.cpp
@@ -24,32 +24,17 @@
 #include <list>
 #include <map>
 
-static size_t getGammaCodeLength(size_t n)
+static size_t writeGammaCode(std::vector< unsigned int >& outBuf, size_t n)
 {
-  size_t  nBits = 1;
+  unsigned int  c = 0U;
+  unsigned char nBits = 1;
   while (n > 1) {
+    c = c | ((2U | ((unsigned int) n & 1U)) << nBits);
     n = n >> 1;
     nBits = nBits + 2;
   }
+  outBuf.push_back(((unsigned int) nBits << 24) | c);
   return nBits;
-}
-
-static void writeGammaCode(std::vector< unsigned int >& outBuf, size_t n)
-{
-  size_t  nBits = 0;
-  size_t  m = 0;
-  while (n > 1) {
-    m = (m << 1) | (n & 1);
-    n = n >> 1;
-    nBits++;
-  }
-  while (nBits > 0) {
-    outBuf.push_back(0x01000001U);
-    outBuf.push_back(0x01000000U | (unsigned int) (m & 1));
-    m = m >> 1;
-    nBits--;
-  }
-  outBuf.push_back(0x01000000U);
 }
 
 static inline size_t estimateSymbolLength(size_t cnt, size_t totalCount)
@@ -63,306 +48,6 @@ static inline size_t estimateSymbolLength(size_t cnt, size_t totalCount)
 }
 
 namespace Ep128Compress {
-
-  Compressor_M0::HuffmanCompressor::HuffmanCompressor(size_t nCharValues_)
-    : nCharValues(nCharValues_)
-  {
-    charCounts.resize(nCharValues);
-    for (size_t i = 0; i < nCharValues; i++)
-      charCounts[i] = 0;
-  }
-
-  Compressor_M0::HuffmanCompressor::~HuffmanCompressor()
-  {
-  }
-
-  void Compressor_M0::HuffmanCompressor::sortNodes(HuffmanNode*& startNode)
-  {
-    size_t  nodeCnt = 0;
-    HuffmanNode *p = startNode;
-    while (p != (HuffmanNode *) 0) {
-      nodeCnt++;
-      p = p->nextNode;
-    }
-    if (nodeCnt < 2)
-      return;
-    size_t  m = nodeCnt >> 1;
-    p = startNode;
-    for (size_t i = 0; i < m; i++) {
-      HuffmanNode *nxt = p->nextNode;
-      if ((i + 1) == m)
-        p->nextNode = (HuffmanNode *) 0;
-      p = nxt;
-    }
-    HuffmanNode *splitNode = p;
-    sortNodes(startNode);
-    sortNodes(splitNode);
-    HuffmanNode *p1 = startNode;
-    HuffmanNode *p2 = splitNode;
-    HuffmanNode *prvNode = (HuffmanNode *) 0;
-    while (true) {
-      if (p1 == (HuffmanNode *) 0) {
-        if (p2 == (HuffmanNode *) 0)
-          break;
-        p = p2;
-        p2 = p2->nextNode;
-      }
-      else if (p2 == (HuffmanNode *) 0) {
-        p = p1;
-        p1 = p1->nextNode;
-      }
-      else if (p2->weight < p1->weight) {
-        p = p2;
-        p2 = p2->nextNode;
-      }
-      else {
-        p = p1;
-        p1 = p1->nextNode;
-      }
-      if (prvNode != (HuffmanNode *) 0)
-        prvNode->nextNode = p;
-      else
-        startNode = p;
-      prvNode = p;
-    }
-    p->nextNode = (HuffmanNode *) 0;
-  }
-
-  void Compressor_M0::HuffmanCompressor::buildEncodeTable(
-      std::vector< unsigned int >& encodeTable,
-      const HuffmanNode *p, unsigned int n = 0U, unsigned int nBits = 0U)
-  {
-    if (p == (HuffmanNode *) 0)
-      return;
-    if (p->isLeafNode()) {
-      encodeTable[p->value] = (nBits << 24) | n;
-    }
-    else {
-      n = n << 1;
-      nBits++;
-      if (p->child0)
-        buildEncodeTable(encodeTable, p->child0, n, nBits);
-      if (p->child1)
-        buildEncodeTable(encodeTable, p->child1, n | 1U, nBits);
-    }
-  }
-
-  void Compressor_M0::HuffmanCompressor::calculateCompression(
-      std::vector< unsigned int >& outBuf,
-      std::vector< unsigned int >& encodeTable)
-  {
-    encodeTable.resize(nCharValues);
-    for (size_t i = 0; i < nCharValues; i++)
-      encodeTable[i] = 0U;
-    size_t  nCharValuesUsed = 0;
-    size_t  totalCharsUncompressed = 0;
-    size_t  totalBitsUncompressed = 0;
-    size_t  totalBitsCompressed = 0;
-    size_t  bitsPerCharUncompressed = 0;
-    {
-      size_t  tmp = 1;
-      while (tmp < nCharValues) {
-        tmp = tmp << 1;
-        bitsPerCharUncompressed++;
-      }
-    }
-    for (size_t i = 0; i < nCharValues; i++) {
-      if (charCounts[i] > 0) {
-        nCharValuesUsed++;
-        totalCharsUncompressed += charCounts[i];
-      }
-    }
-    totalBitsUncompressed = totalCharsUncompressed * bitsPerCharUncompressed;
-    // check for trivial cases (zero or one symbol only)
-    if (nCharValuesUsed == 0) {
-      outBuf.push_back(0x01000000U);
-      return;
-    }
-    if (nCharValuesUsed == 1) {
-      for (size_t i = 0; i < nCharValues; i++) {
-        if (charCounts[i] > 0)
-          encodeTable[i] = 0x01000000U;
-      }
-    }
-    else {
-      // build Huffman tree
-      std::vector< HuffmanNode >  buf;
-      buf.resize(nCharValues * 2);
-      HuffmanNode *nodeList1 = (HuffmanNode *) 0;
-      HuffmanNode *nodeList1End = (HuffmanNode *) 0;
-      HuffmanNode *nodeList2 = (HuffmanNode *) 0;
-      HuffmanNode *nodeList2End = (HuffmanNode *) 0;
-      size_t  n = 0;
-      for (size_t i = 0; i < nCharValues; i++) {
-        if (charCounts[i] > 0) {
-          HuffmanNode *p = &(buf[n]);
-          n++;
-          p->weight = charCounts[i];
-          p->value = i;
-          p->parent = (HuffmanNode *) 0;
-          p->child0 = (HuffmanNode *) 0;
-          p->child1 = (HuffmanNode *) 0;
-          p->nextNode = (HuffmanNode *) 0;
-          if (nodeList1End != (HuffmanNode *) 0)
-            nodeList1End->nextNode = p;
-          else
-            nodeList1 = p;
-          nodeList1End = p;
-        }
-      }
-      sortNodes(nodeList1);
-      nodeList1End = nodeList1;
-      if (nodeList1 != (HuffmanNode *) 0) {
-        while (nodeList1End->nextNode != (HuffmanNode *) 0)
-          nodeList1End = nodeList1End->nextNode;
-      }
-      HuffmanNode *rootNode = (HuffmanNode *) 0;
-      while (true) {
-        HuffmanNode *p1 = (HuffmanNode *) 0;
-        if (nodeList1 == (HuffmanNode *) 0) {
-          if (nodeList2 == (HuffmanNode *) 0)
-            break;
-          p1 = nodeList2;
-          nodeList2 = nodeList2->nextNode;
-          if (nodeList2 == (HuffmanNode *) 0)
-            nodeList2End = (HuffmanNode *) 0;
-        }
-        else if (nodeList2 == (HuffmanNode *) 0) {
-          p1 = nodeList1;
-          nodeList1 = nodeList1->nextNode;
-          if (nodeList1 == (HuffmanNode *) 0)
-            nodeList1End = (HuffmanNode *) 0;
-        }
-        else if (nodeList2->weight < nodeList1->weight) {
-          p1 = nodeList2;
-          nodeList2 = nodeList2->nextNode;
-          if (nodeList2 == (HuffmanNode *) 0)
-            nodeList2End = (HuffmanNode *) 0;
-        }
-        else {
-          p1 = nodeList1;
-          nodeList1 = nodeList1->nextNode;
-          if (nodeList1 == (HuffmanNode *) 0)
-            nodeList1End = (HuffmanNode *) 0;
-        }
-        HuffmanNode *p0 = (HuffmanNode *) 0;
-        if (nodeList1 == (HuffmanNode *) 0) {
-          if (nodeList2 == (HuffmanNode *) 0) {
-            rootNode = p1;
-            break;
-          }
-          p0 = nodeList2;
-          nodeList2 = nodeList2->nextNode;
-          if (nodeList2 == (HuffmanNode *) 0)
-            nodeList2End = (HuffmanNode *) 0;
-        }
-        else if (nodeList2 == (HuffmanNode *) 0) {
-          p0 = nodeList1;
-          nodeList1 = nodeList1->nextNode;
-          if (nodeList1 == (HuffmanNode *) 0)
-            nodeList1End = (HuffmanNode *) 0;
-        }
-        else if (nodeList2->weight < nodeList1->weight) {
-          p0 = nodeList2;
-          nodeList2 = nodeList2->nextNode;
-          if (nodeList2 == (HuffmanNode *) 0)
-            nodeList2End = (HuffmanNode *) 0;
-        }
-        else {
-          p0 = nodeList1;
-          nodeList1 = nodeList1->nextNode;
-          if (nodeList1 == (HuffmanNode *) 0)
-            nodeList1End = (HuffmanNode *) 0;
-        }
-        rootNode = &(buf[n]);
-        n++;
-        rootNode->weight = p0->weight + p1->weight;
-        rootNode->value = 0;
-        rootNode->parent = (HuffmanNode *) 0;
-        rootNode->child0 = p0;
-        p0->parent = rootNode;
-        rootNode->child1 = p1;
-        p1->parent = rootNode;
-        rootNode->nextNode = (HuffmanNode *) 0;
-        if (nodeList2End != (HuffmanNode *) 0)
-          nodeList2End->nextNode = rootNode;
-        else
-          nodeList2 = rootNode;
-        nodeList2End = rootNode;
-      }
-      buildEncodeTable(encodeTable, rootNode);
-      buf.clear();
-    }
-    // convert encode table to canonical Huffman codes
-    size_t  sizeCounts[16];
-    size_t  sizeCodes[16];
-    for (size_t i = 0; i < 16; i++)
-      sizeCounts[i] = 0;
-    for (size_t i = 0; i < nCharValues; i++) {
-      if (charCounts[i] > 0)
-        sizeCounts[(encodeTable[i] >> 24) - 1U]++;
-    }
-    sizeCodes[0] = 0;
-    for (size_t i = 1; i < 16; i++)
-      sizeCodes[i] = (sizeCodes[i - 1] + sizeCounts[i - 1]) << 1;
-    for (size_t i = 0; i < nCharValues; i++) {
-      if (charCounts[i] > 0) {
-        size_t  j = size_t(encodeTable[i] >> 24);
-        encodeTable[i] = (unsigned int) ((j << 24) | sizeCodes[j - 1]);
-        sizeCodes[j - 1]++;
-      }
-    }
-    // avoid the code 1111111 since it currently breaks the decompressor
-    for (size_t i = 0; i < nCharValues; i++) {
-      if (encodeTable[i] == 0x0700007FU) {
-        encodeTable[i] = 0x080000FEU;
-        break;
-      }
-    }
-    // calculate compressed size
-    for (size_t i = 1; i <= 16; i++) {
-      unsigned int  prvChar = 0U - 1U;
-      size_t        sizeCnt = 0;
-      for (size_t j = 0; j < nCharValues; j++) {
-        if (size_t(encodeTable[j] >> 24) == i) {
-          sizeCnt++;
-          unsigned int  newChar = (unsigned int) j;
-          unsigned int  d = newChar - prvChar;
-          prvChar = newChar;
-          totalBitsCompressed += getGammaCodeLength(d);
-        }
-      }
-      totalBitsCompressed += getGammaCodeLength(sizeCnt + 1);
-    }
-    // if the size cannot be reduced, store the data without compression
-    if (totalBitsCompressed >= totalBitsUncompressed) {
-      for (size_t i = 0; i < nCharValues; i++)
-        encodeTable[i] = (unsigned int) ((bitsPerCharUncompressed << 24) | i);
-      outBuf.push_back(0x01000000U);
-      return;
-    }
-    // add decode table to the output buffer
-    outBuf.push_back(0x01000001U);
-    for (size_t i = 1; i <= 16; i++) {
-      size_t  sizeCnt = 0;
-      for (size_t j = 0; j < nCharValues; j++) {
-        if (size_t(encodeTable[j] >> 24) == i)
-          sizeCnt++;
-      }
-      writeGammaCode(outBuf, sizeCnt + 1);
-      unsigned int  prvChar = 0U - 1U;
-      for (size_t j = 0; j < nCharValues; j++) {
-        if (size_t(encodeTable[j] >> 24) == i) {
-          unsigned int  newChar = (unsigned int) j;
-          unsigned int  d = newChar - prvChar;
-          prvChar = newChar;
-          writeGammaCode(outBuf, d);
-        }
-      }
-    }
-  }
-
-  // --------------------------------------------------------------------------
 
   Compressor_M0::DSearchTable::DSearchTable(size_t minLength, size_t maxLength,
                                             size_t maxOffs)
@@ -422,36 +107,86 @@ namespace Ep128Compress {
 
   // --------------------------------------------------------------------------
 
+  static size_t calculateHuffmanEncoding(
+      std::vector< unsigned int >& outBuf,
+      const std::vector< unsigned int >& inBuf,
+      HuffmanEncoder& huffmanEncoder, std::vector< unsigned int >& encodeTable)
+  {
+    unsigned int  symbolCnts[324];
+    size_t  n = inBuf.size();
+    size_t  totalCnt = 0;
+    size_t  nSymbols = encodeTable.size();
+    for (size_t i = 0; i < nSymbols; i++)
+      symbolCnts[i] = 0U;
+    huffmanEncoder.clear();
+    unsigned int  symOffs = (nSymbols < 256 ? 0x180U : 0U);
+    for (size_t i = 4; i < n; i++) {
+      unsigned int  c = inBuf[i] - symOffs;
+      if (c < 324U) {
+        symbolCnts[c] = symbolCnts[c] + 1U;
+        totalCnt++;
+        huffmanEncoder.addSymbol(c);
+      }
+    }
+    size_t  uncompressedSize = totalCnt * size_t(nSymbols < 256 ? 5 : 9) + 1;
+    size_t  compressedSize = 1;
+    // create optimal encoding table
+    huffmanEncoder.updateTables(false, 16);
+    for (unsigned int c = 0U; c < (unsigned int) nSymbols; c++) {
+      if (huffmanEncoder.getSymbolSize(c) <= 16) {
+        unsigned int  tmp = huffmanEncoder.encodeSymbol(c);
+        // avoid the code 1111111b, since it currently breaks the decompressor
+        tmp = (tmp != 0x0700007FU ? tmp : 0x080000FEU);
+        encodeTable[c] = tmp;
+        compressedSize += size_t(symbolCnts[c] * (tmp >> 24));
+      }
+    }
+    // create decode table to be written to the output buffer,
+    // and calculate its size
+    outBuf.push_back(0x01000001U);
+    size_t  savedBufSize = outBuf.size();
+    for (unsigned int l = 1U; l <= 16U; l++) {
+      size_t  sizeCnt = 0;
+      for (unsigned int c = 0U; c < nSymbols; c++)
+        sizeCnt += size_t((encodeTable[c] >> 24) == l);
+      compressedSize += writeGammaCode(outBuf, sizeCnt + 1);
+      unsigned int  prvCode = 0U - 1U;
+      for (unsigned int c = 0U; c < nSymbols; c++) {
+        if ((encodeTable[c] >> 24) == l) {
+          size_t  d = size_t(c - prvCode);
+          prvCode = c;
+          compressedSize += writeGammaCode(outBuf, d);
+        }
+      }
+    }
+    // if the size cannot be reduced, store the data without compression
+    if (compressedSize >= uncompressedSize) {
+      outBuf.resize(savedBufSize);
+      outBuf[savedBufSize - 1] = 0x01000000U;
+      for (unsigned int i = 0U; i < (unsigned int) nSymbols; i++)
+        encodeTable[i] = (nSymbols < 256 ? 0x05000000U : 0x09000000U) | i;
+    }
+    return totalCnt;
+  }
+
   void Compressor_M0::huffmanCompressBlock(std::vector< unsigned int >& ioBuf)
   {
-    HuffmanCompressor   huff1(324);
-    HuffmanCompressor   huff2(28);
     size_t  n = ioBuf.size();
     if (n <= 4)
       return;
-    size_t  charCnt1 = 0;
-    size_t  charCnt2 = 0;
-    for (size_t i = 4; i < n; i++) {
-      if (ioBuf[i] <= 0x17FU) {
-        charCnt1++;
-        huff1.addChar(ioBuf[i]);
-      }
-      else if (ioBuf[i] <= 0x1FFU) {
-        charCnt2++;
-        huff2.addChar(ioBuf[i] - 0x180U);
-      }
-    }
     std::vector< unsigned int > encodeTable1(324, 0U);
     std::vector< unsigned int > encodeTable2(28, 0U);
     std::vector< unsigned int > tmpBuf;
-    huff1.calculateCompression(tmpBuf, encodeTable1);
-    huff2.calculateCompression(tmpBuf, encodeTable2);
+    size_t  charCnt1 =
+        calculateHuffmanEncoding(tmpBuf, ioBuf, huffmanEncoder1, encodeTable1);
+    size_t  charCnt2 =
+        calculateHuffmanEncoding(tmpBuf, ioBuf, huffmanEncoder2, encodeTable2);
     ioBuf.resize(n + tmpBuf.size());    // reserve space for the decode table
     for (size_t i = n; i-- > 4; ) {
       if (ioBuf[i] <= 0x17FU)
         ioBuf[i + tmpBuf.size()] = encodeTable1[ioBuf[i]];
       else if (ioBuf[i] <= 0x1FFU)
-        ioBuf[i + tmpBuf.size()] = encodeTable2[ioBuf[i] - 0x180U];
+        ioBuf[i + tmpBuf.size()] = encodeTable2[ioBuf[i] & 0x7FU];
       else
         ioBuf[i + tmpBuf.size()] = ioBuf[i];
     }
@@ -691,8 +426,8 @@ namespace Ep128Compress {
 
   void Compressor_M0::compressData_(std::vector< unsigned int >& tmpOutBuf,
                                     const std::vector< unsigned char >& inBuf,
-                                    unsigned int startAddr, bool isLastBlock,
-                                    size_t offs, size_t nBytes)
+                                    unsigned int startAddr, size_t offs,
+                                    size_t nBytes)
   {
     size_t  endPos = offs + nBytes;
     // write data header (start address, 2's complement of the number of bytes,
@@ -703,12 +438,12 @@ namespace Ep128Compress {
       tmpOutBuf.push_back(0x10000000U | (unsigned int) (65536 - nBytes));
     }
     else {
-      // hack to keep the number of header symbols constant
+      // hack to keep the header size constant
       tmpOutBuf.push_back(0x08000000U | (unsigned int) ((65536 - nBytes) >> 8));
       tmpOutBuf.push_back(0x08000000U
                           | (unsigned int) ((65536 - nBytes) & 0xFF));
     }
-    tmpOutBuf.push_back(isLastBlock ? 0x01000001U : 0x01000000U);
+    tmpOutBuf.push_back(0x01000000U);   // reserved for last block flag
     tmpOutBuf.push_back(0x01000001U);
     // compress data by searching for repeated byte sequences,
     // and replacing them with length/distance codes
@@ -776,7 +511,7 @@ namespace Ep128Compress {
       if (doneFlag)     // if the compression cannot be optimized further,
         continue;       // quit the loop earlier
       tmpBuf.clear();
-      compressData_(tmpBuf, inBuf, startAddr, false, offs, nBytes);
+      compressData_(tmpBuf, inBuf, startAddr, offs, nBytes);
       // apply statistical compression
       huffmanCompressBlock(tmpBuf);
       // calculate compressed size and hash value
@@ -840,7 +575,9 @@ namespace Ep128Compress {
       tmpCharBitsTable((size_t *) 0),
       searchTable((DSearchTable *) 0),
       outputShiftReg(0x00),
-      outputBitCnt(0)
+      outputBitCnt(0),
+      huffmanEncoder1(324, 0),
+      huffmanEncoder2(28, 0)
   {
     try {
       lengthCodeTable = new unsigned short[maxRepeatLen + 1];
@@ -1056,11 +793,8 @@ namespace Ep128Compress {
       for (size_t i = 0; i < outBufTmp.size(); i++) {
         unsigned int  c = outBufTmp[i];
         unsigned int  nBits = c >> 24;
-        if (nBits > 16U)
-          throw Ep128Emu::Exception("internal error: Huffman code > 16 bits");
         c = c & 0x00FFFFFFU;
-        for (unsigned int j = nBits; j > 0U; ) {
-          j--;
+        for (unsigned int j = nBits; j-- > 0U; ) {
           unsigned int  b = (unsigned int) (bool(c & (1U << j)));
           outputShiftReg = ((outputShiftReg & 0x7F) << 1) | (unsigned char) b;
           if (++outputBitCnt >= 8) {
