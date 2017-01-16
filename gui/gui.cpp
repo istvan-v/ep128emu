5229daace5070441663f229c3c53f2ba4236279d
IstvanV <istvan-v@users.noreply.github.com>
2017-01-16 10:57:57 +0100

    2.0.11 release

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -131,10 +131,10 @@ void Ep128EmuGUI::init_()
 void Ep128EmuGUI::updateDisplay_windowTitle()
 {
   if (oldPauseFlag) {
-    std::sprintf(&(windowTitleBuf[0]), "ep128emu 2.0.11 beta (paused)");
+    std::sprintf(&(windowTitleBuf[0]), "ep128emu 2.0.11 (paused)");
   }
   else {
-    std::sprintf(&(windowTitleBuf[0]), "ep128emu 2.0.11 beta (%d%%)",
+    std::sprintf(&(windowTitleBuf[0]), "ep128emu 2.0.11 (%d%%)",
                  int(oldSpeedPercentage));
   }
   mainWindow->label(&(windowTitleBuf[0]));
